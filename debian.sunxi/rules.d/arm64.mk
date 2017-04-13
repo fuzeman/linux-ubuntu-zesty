bf0239f96f76420255182001f831af47486649cf
Dean Gardiner <me@dgardiner.net>
2017-04-13 16:14:01 +1200

    debian.sunxi: updated architecture rules

--- debian.sunxi/rules.d/arm64.mk
+++ debian.sunxi/rules.d/arm64.mk
@@ -1,24 +1,20 @@
-human_arch	= ARMv8
-build_arch	= arm64
-header_arch	= arm64
-defconfig	= defconfig
-flavours	= sunxi sunxi-extra
-build_image	= Image
-kernel_file	= arch/$(build_arch)/boot/Image
-install_file	= vmlinuz
-no_dumpfile	= true
+human_arch	        = ARMv8
+build_arch	        = arm64
+header_arch	        = arm64
+defconfig	        = defconfig
+flavours	        = nanopi-a64 pine-a64 sunxi sunxi-extra
+build_image	        = Image.gz
+kernel_file	        = arch/$(build_arch)/boot/Image.gz
+install_file	    = vmlinuz
+no_dumpfile	        = true
 
-loader		= grub
+loader		        = grub
 
-do_linux_tools	= true
-do_tools_usbip  = true
-do_tools_cpupower = true
-do_tools_perf	= true
+do_extras_package   = true
+do_linux_tools	    = true
+do_tools_usbip      = true
+do_tools_cpupower   = true
+do_tools_perf	    = true
 
-do_common_headers_indep=false
-
-do_dtbs		= true
-disable_d_i = true
-do_libc_dev_package=false
-do_doc_package	= false
-do_source_package= false
+do_dtbs		        = true
+do_libc_dev_package = false
