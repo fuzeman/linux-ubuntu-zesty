e3a424480ddcf4105c42fe035987a27f68c91d03
Dean Gardiner <me@dgardiner.net>
2017-04-17 14:12:18 +1200

    debian.sunxi: updated architecture rules

--- debian.sunxi/rules.d/arm64.mk
+++ debian.sunxi/rules.d/arm64.mk
@@ -1,20 +1,23 @@
-human_arch	        = ARMv8
-build_arch	        = arm64
-header_arch	        = arm64
-defconfig	        = defconfig
-flavours	        = nanopi-a64 pine-a64 sunxi sunxi-extra
-build_image	        = Image.gz
-kernel_file	        = arch/$(build_arch)/boot/Image.gz
-install_file	    = vmlinuz
-no_dumpfile	        = true
+human_arch	        	= ARMv8
+build_arch	        	= arm64
+header_arch	        	= arm64
+defconfig	        	= defconfig
+flavours	        	= nanopi-a64 pine-a64 sunxi sunxi-extra
+build_image	        	= Image.gz
+kernel_file	        	= arch/$(build_arch)/boot/Image.gz
+install_file	    	= vmlinuz
+no_dumpfile	        	= true
 
-loader		        = grub
+loader		        	= grub
 
-do_extras_package   = true
-do_linux_tools	    = true
-do_tools_usbip      = true
-do_tools_cpupower   = true
-do_tools_perf	    = true
+do_linux_tools	    	= true
+do_tools_usbip      	= true
+do_tools_cpupower   	= true
+do_tools_perf	    	= true
 
-do_dtbs		        = true
-do_libc_dev_package = false
+do_common_headers_indep = false
+disable_d_i 			= true
+do_dtbs		        	= true
+do_libc_dev_package 	= false
+do_doc_package			= false
+do_source_package		= false
--- debian.sunxi/rules.d/armhf.mk
+++ debian.sunxi/rules.d/armhf.mk
@@ -1,20 +1,20 @@
-human_arch	        = ARM (hard float)
-build_arch	        = arm
-header_arch	        = arm
-defconfig	        = defconfig
-flavours	        = nanopi-neo nanopi-neo-air orangepi-zero sunxi sunxi-extra
-build_image	        = zImage
-kernel_file	        = arch/$(build_arch)/boot/zImage
-install_file	    = vmlinuz
-no_dumpfile	        = true
+human_arch	        	= ARM (hard float)
+build_arch	        	= arm
+header_arch	        	= arm
+defconfig	        	= defconfig
+flavours	        	= nanopi-neo nanopi-neo-air orangepi-zero sunxi sunxi-extra
+build_image	        	= zImage
+kernel_file	        	= arch/$(build_arch)/boot/zImage
+install_file	    	= vmlinuz
+no_dumpfile	        	= true
 
-loader		        = grub
+loader		        	= grub
 
-do_extras_package   = true
-do_linux_tools	    = true
-do_tools_usbip      = true
-do_tools_cpupower   = true
-do_tools_perf	    = true
+do_linux_tools	    	= true
+do_tools_usbip      	= true
+do_tools_cpupower   	= true
+do_tools_perf	    	= true
 
-do_dtbs		        = true
-do_libc_dev_package = false
+do_common_headers_indep = false
+do_dtbs		        	= true
+do_libc_dev_package 	= false
